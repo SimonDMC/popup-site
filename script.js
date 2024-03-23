f59224d92620b07ff2c21b52a5de6d6509bc9b6a
SimonDMC <simondmcplayer@gmail.com>
2024-03-23 15:26:55 +0100

    Bump lib version to 1.4.3

--- script.js
+++ script.js
@@ -17,12 +17,11 @@ const popups = [
         Six characters, ranging from 00-FF for 3 channels.
         Values are in {a-http://example.com}[Base-16].
         big-margin§{bold}[#{red}[E4]{green}[F2]{blue}[DB]]
-        big-margin§{black bold}[#000000] is black.                {white bold shadow}[#FFFFFF] is white.
+        big-margin§{black bold}[#000000] is black.             {white bold shadow}[#FFFFFF] is white.
         big-margin§Good luck.`,
         titleColor: "rgb(92, 0, 95)",
         titleMargin: "0",
         backgroundColor: "#ffebfe",
-        fixedHeight: true,
     }),
     new Popup({
         id: "download",
